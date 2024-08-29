4ecae5db81a256a8b2100bd7ed66b9b008fc542b
MehdiSamid <96839827+MehdiSamid@users.noreply.github.com>
2024-08-29 19:10:15 +0100

    init

---
+++ translation.js
@@ -0,0 +1,111 @@
+const translations = {
+  en: {
+    fre: 'French',
+    eng: 'English',
+    home: 'Home',
+    about: 'About',
+    facts: 'Facts',
+    skills: 'Skills',
+    resume: 'Resume',
+    portfolio: 'Portfolio',
+    contact: 'Contact',
+    degree: "bachelor's",
+    av: "Available",
+    im: "I'm",
+    about_title: "About",
+    about_content: "🚀 a skilled full-stack developer with +2 years of experience. I've crafted 10+ websites and 2 Flutter apps, turning ideas into digital reality. Let's build something extraordinary together! 💻📱",
+    developer_title: "Web Developer.",
+    portfolio_description: "Feel free to explore my portfolio to get a glimpse of my work, and don't hesitate to get in touch if you have a project in mind. Let's make your digital dreams a reality!",
+    about_description: "I'm El Mehdi Samid, a passionate full-stack developer. With over 2 years of experience in the world of web and app development, I've had the privilege of crafting more than 10 successful websites and 2 cutting-edge Flutter apps. My journey through the digital landscape has been driven by a deep fascination for technology and a commitment to creating elegant solutions that empower businesses and individuals."
+    , collaboration_description: "Whether you're a startup with a vision, an established business seeking to enhance your online presence, or someone with a brilliant app idea, I'm here to collaborate with you. Together, we can build something amazing."
+    , summary_title: "Summary",
+    resume_description: "a skilled Full-Stack Developer with a successful track record of building over 10 websites. Eager and ready to contribute my expertise and passion to your team."
+    , education_title: "Education",
+    technical_specialist: "Technical Specialist",
+    ista_ntic: "ISTA NTIC 2 Sidi Maarouf",
+    computer_science: "Specialization in Computer Science",
+    bachelors_degree: "Bachelor's Degree",
+    university_name: "Université Hassan 2 de Casablanca 'FST'",
+    application_design: "Professional University Degree in Application Design and Development",
+    professional_experience_title: "Professional Experience",
+    junior_developer_title: "Junior Developer",
+    company_dsics: "DSI CONSEIL & SERVICES, Casablanca",
+    integration_mockups: "Integrating mockups from designers, drafting specifications (technical/functional), and project design.",
+    creating_project_structure: "Creating the project structure.",
+    modularization_design_integration: "Modularization and design integration.",
+    analyzing_existing_system: "Analyzing the existing system and adding new features.",
+    working_on_projects: "Working on +4 big projects.",
+    java_expert_assistant_title: "Java Expert Assistant",
+    internship_duration_1: "1 Month (Internship)",
+    internship_duration_2: "2 Month (Internship)",
+    company_marsa_maroc: "Marsa Maroc, Casablanca",
+    internship_tasks_1: "During my one-month internship as an Assistant Java Expert, I actively assisted in Java development tasks, gaining hands-on experience and enhancing my proficiency in the field.",
+    company_allianz_assurance: "ALLIANZ ASSURANCE, Casablanca",
+    internship_tasks_2: "Internship in the ALLIANZ ASSURANCE application maintenance and monitoring.",
+    portfolio_title: "Portfolio",
+    portfolio_description: "I've worked on the projects listed below, some as a freelancer and others as an employee.",
+    bring_projects_to_life: "Let's bring your digital projects to life!"
+
+  },
+  fr: {
+    fre: 'Francais',
+    eng: 'Anglais',
+    home: 'Accueil',
+    about: 'À propos',
+    facts: 'Faits',
+    skills: 'Compétences',
+    resume: 'CV',
+    portfolio: 'Portfolio',
+    contact: 'Contact',
+    degree: "Licence",
+    av: "Disponible",
+    im: 'Je suis',
+    about_title: "À propos",
+    about_content: "🚀 un développeur full-stack compétent avec plus de 2 ans d'expérience. J'ai conçu plus de 10 sites web et 2 applications Flutter, transformant les idées en réalité numérique. Construisons quelque chose d'extraordinaire ensemble ! 💻📱",
+    developer_title: "Développeur Web.",
+    portfolio_description: "N'hésitez pas à explorer mon portfolio pour avoir un aperçu de mon travail, et n'hésitez pas à me contacter si vous avez un projet en tête. Faisons de vos rêves numériques une réalité !",
+    about_description: "Je suis El Mehdi Samid, un développeur full-stack passionné. Avec plus de 2 ans d'expérience dans le monde du développement web et d'applications, j'ai eu le privilège de créer plus de 10 sites Web réussis et 2 applications Flutter de pointe. Mon parcours à travers le paysage numérique a été guidé par une profonde fascination pour la technologie et un engagement à créer des solutions élégantes qui donnent du pouvoir aux entreprises et aux individus.",
+    collaboration_description: "Que vous soyez une start-up avec une vision, une entreprise établie cherchant à améliorer votre présence en ligne, ou quelqu'un avec une idée d'application brillante, je suis là pour collaborer avec vous. Ensemble, nous pouvons construire quelque chose d'extraordinaire.",
+    summary_title: "Résumé",
+    resume_description: "un développeur Full-Stack compétent avec un bilan réussi de construction de plus de 10 sites Web. Impatient et prêt à apporter mon expertise et ma passion à votre équipe."
+    , education_title: "Éducation",
+    technical_specialist: "téchnicien spécialisé",
+    ista_ntic: "ISTA NTIC 2 Sidi Maarouf",
+    computer_science: "Spécialisation en développement Informatique",
+    bachelors_degree: "Licence",
+    university_name: "Université Hassan 2 de Casablanca 'FST'",
+    application_design: "Diplôme Universitaire Professionnel en Conception et Développement d'Applications"
+    , professional_experience_title: "Expérience Professionnelle",
+    junior_developer_title: "Développeur Junior",
+    company_dsics: "DSI CONSEIL & SERVICES, Casablanca",
+    integration_mockups: "Intégration des maquettes des designers, rédaction de spécifications (techniques/fonctionnelles) et conception de projets.",
+    creating_project_structure: "Création de la structure du projet.",
+    modularization_design_integration: "Modularisation et intégration de la conception.",
+    analyzing_existing_system: "Analyse du système existant et ajout de nouvelles fonctionnalités.",
+    working_on_projects: "Travail sur +4 grands projets.",
+    java_expert_assistant_title: "Assistant Expert Java",
+    internship_duration_1: "1 Mois (Stage)",
+    internship_duration_2: "2 Mois (Stage)",
+    company_marsa_maroc: "Marsa Maroc, Casablanca",
+    internship_tasks_1: "Pendant mon stage d'un mois en tant qu'Assistant Expert Java, j'ai activement assisté dans les tâches de développement Java, acquérant ainsi une expérience pratique et améliorant ma maîtrise dans le domaine.",
+    company_allianz_assurance: "ALLIANZ ASSURANCE, Casablanca",
+    internship_tasks_2: "Stage dans la maintenance et le suivi de l'application ALLIANZ ASSURANCE.",
+    portfolio_title: "Portfolio",
+    portfolio_description: "J'ai travaillé sur les projets listés ci-dessous, certains en tant que freelance et d'autres en tant qu'employé.",
+    bring_projects_to_life: "Donnons vie à vos projets numériques !",
+
+
+  }
+};
+
+function changeLanguage(language) {
+  const translation = translations[language];
+  const elements = document.querySelectorAll("[data-translate]");
+
+  elements.forEach(element => {
+    const key = element.getAttribute("data-translate");
+    if (translation[key]) {
+      element.textContent = translation[key];
+    }
+  });
+}
